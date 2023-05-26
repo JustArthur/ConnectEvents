c40bbe31350e1dce5cab54f8f4d3d49a80e63b8d
su apt install kuma <92596486+JustArthur@users.noreply.github.com>
2023-05-26 11:41:06 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/JustArthur/ConnectEvents.git"
+$url = "https://github.com/JustArthur/ConnectEvents/edit/main/install.ps1"
 $outputPath = "C:\wamp64\www\"
 
 git clone $url $outputPath
