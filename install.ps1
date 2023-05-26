ce503de86726e50796728f81746460dd50fad1f2
su apt install kuma <92596486+JustArthur@users.noreply.github.com>
2023-05-26 17:39:42 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -17,14 +17,15 @@ foreach ($file in $excludedFiles) {
     $fullPath = Join-Path -Path $outputPath -ChildPath $file
     if ((Test-Path $fullPath) -or (Test-Path ($fullPath + ".*"))) {
         Remove-Item -Path $fullPath -Force -Recurse
+        $successCount++
     }
 }
 
 if ($successCount -eq $excludedFiles.Count) {
-    Write-Host "Tu{00E9}lu{00E9}chargement reussi." -ForegroundColor Green
+    Write-Host "Telechargement reussi." -ForegroundColor Green
     Write-Host " "
-    Write-Host "Merci d'avoir tu{00E9}lu{00E9}echarge ConnectEvent !" -ForegroundColor Blue
+    Write-Host "Merci d'avoir telecharge ConnectEvent !" -ForegroundColor Blue
     
 } else {
-    Write-Host "Certains fichiers n'ont pas pu etre tu{00E9}lu{00E9}charger." -ForegroundColor Red
+    Write-Host "Certains fichiers n'ont pas pu etre telecharger." -ForegroundColor Red
 }
