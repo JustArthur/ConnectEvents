075ee7c372418c606740f48c44a1e0af0a1f42fa
su apt install Kuma <maxxozou@gmail.com>
2023-05-26 12:16:37 +0200

    Ver 1.1.6.1
    Modification du install.ps1 pour éviter des installations inutile.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,24 @@
 $url = "https://github.com/JustArthur/ConnectEvents.git"
 $outputPath = "C:\wamp64\www\ConnectEvents"
 
+# Effectuer le clone initial
 git clone $url $outputPath
+
+# Supprimer les fichiers et dossiers indésirables
+$excludedFiles = @(
+    "screenshot/",
+    "configuration-messagerie.md",
+    "DataBase_ConnectEvents.sql",
+    "install.ps1",
+    "LICENSE",
+    "mit_license.md",
+    "README.md",
+    ".gitignore"
+)
+
+foreach ($file in $excludedFiles) {
+    $fullPath = Join-Path -Path $outputPath -ChildPath $file
+    if (Test-Path $fullPath -or Test-Path ($fullPath + ".*")) {
+        Remove-Item -Path $fullPath -Force -Recurse
+    }
+}
