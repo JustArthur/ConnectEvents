65e92f503572701588ced8a5456f151fc3264c96
su apt install kuma <92596486+JustArthur@users.noreply.github.com>
2023-05-26 17:36:25 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -21,10 +21,10 @@ foreach ($file in $excludedFiles) {
 }
 
 if ($successCount -eq $excludedFiles.Count) {
-    Write-Host "Telechargement reussi." -ForegroundColor Green
+    Write-Host "Tu{00E9}lu{00E9}chargement reussi." -ForegroundColor Green
     Write-Host " "
-    Write-Host "Merci d'avoir telecharge ConnectEvent !" -ForegroundColor Blue
+    Write-Host "Merci d'avoir tu{00E9}lu{00E9}echarge ConnectEvent !" -ForegroundColor Blue
     
 } else {
-    Write-Host "Certains fichiers n'ont pas pu etre telecharger." -ForegroundColor Red
+    Write-Host "Certains fichiers n'ont pas pu etre tu{00E9}lu{00E9}charger." -ForegroundColor Red
 }
